6fdfb2e56154dd8d701be684bcf18b31345423ef
Erik Vullings <erik.vullings@gmail.com>
2019-07-19 23:19:13 +0200

    Change import

--- packages/example/src/components/form-view.ts
+++ packages/example/src/components/form-view.ts
@@ -1,5 +1,5 @@
 import m from 'mithril';
-import { LayoutForm, Form, SlimdownView, IObject } from 'mithril-ui-form';
+import { LayoutForm, Form, SlimdownView } from 'mithril-ui-form';
 import { TextArea } from 'mithril-materialized';
 
 export interface IContext {
