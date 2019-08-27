caeca568d2d1673712b02fea517d9cf315122887
Erik Vullings <erik.vullings@gmail.com>
2019-08-27 09:34:08 +0200

    FIX context did not contain original object in RepeatItem

--- packages/mithril-ui-form/src/components/repeat-list.ts
+++ packages/mithril-ui-form/src/components/repeat-list.ts
@@ -103,7 +103,7 @@ export const RepeatList: FactoryComponent<IRepeatList> = () => {
                       state.editModal.open();
                     }
                   },
-                  context,
+                  context: [obj, context],
                   section,
                 })
               )
