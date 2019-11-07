3ecc676088ee2c69056dc804a5da6d31274eee71
Erik Vullings <erik.vullings@gmail.com>
2019-11-07 17:44:51 +0100

    removed console.log - patch releases 0.4.4

--- packages/mithril-ui-form/src/components/repeat-list.ts
+++ packages/mithril-ui-form/src/components/repeat-list.ts
@@ -126,7 +126,6 @@ export const RepeatList: FactoryComponent<IRepeatList> = () => {
         : () => true;
       const regex = new RegExp(`\\??\\&?${id}=\\d+`);
       const route = m.route.get().replace(regex, '');
-      console.table({ route, routeGet: m.route.get() });
       const maxPages = pageSize ? Math.ceil(items.length / pageSize) : 0;
       // console.table({ id: field.id, page, curPage, maxPages, length: items.length });
 
