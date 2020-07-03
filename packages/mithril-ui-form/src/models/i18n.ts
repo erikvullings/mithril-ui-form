a1e20c7aa1a6ee7a509861e09bf9371c85b13416
Erik Vullings <erik.vullings@gmail.com>
2020-07-03 18:09:42 +0200

    Improve localization support

--- packages/mithril-ui-form/src/models/i18n.ts
+++ packages/mithril-ui-form/src/models/i18n.ts
@@ -1,7 +1,21 @@
 /** Contains all the translatable keys */
 export interface I18n {
   /** Label for the edit button of the RepeatList */
-  editRepeat: string;
+  editRepeat?: string;
   /** Label for the create button of the RepeatList */
-  createRepeat: string;
+  createRepeat?: string;
+  /** Label for the delete button of the RepeatList */
+  deleteItem?: string;
+  /** Label for the agree button of the RepeatList */
+  agree?: string;
+  /** Label for the disagree button of the RepeatList */
+  disagree?: string;
+  /** Pick one */
+  pickOne?: string;
+  /** Pick one or more */
+  pickOneOrMore?: string;
+  /** Cancel button text */
+  cancel?: string;
+  /** Save button text */
+  save?: string;
 }
