6a93beda2c444e03900e86b23bc262d0b9428bc8
erikvullings <erik.vullings@gmail.com>
2020-11-18 20:05:15 +0100

    SlimdownView: add option to remove paragraphs

--- packages/mithril-ui-form/src/components/slimdown-view.ts
+++ packages/mithril-ui-form/src/components/slimdown-view.ts
@@ -3,11 +3,12 @@ import { render } from 'slimdown-js';
 
 export interface IMarkdownView extends Attributes {
   md?: string;
+  removeParagraphs?: boolean;
 }
 
 export const SlimdownView: FactoryComponent<IMarkdownView> = () => {
   return {
-    view: ({ attrs: { md, className = '.col.s12' } }) =>
-      md ? m(`.markdown ${className}`, m.trust(render(md))) : undefined,
+    view: ({ attrs: { md, removeParagraphs = false, className = '.col.s12' } }) =>
+      md ? m(`.markdown ${className}`, m.trust(render(md, removeParagraphs))) : undefined,
   };
 };
